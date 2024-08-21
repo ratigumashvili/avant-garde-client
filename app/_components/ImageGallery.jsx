4089f3519e5726319f36b716006178922a8b5029
ratigumashvili <ratigumashvili@gmail.com>
2024-08-21 15:33:28 +0400

    + image gallery

---
+++ app/_components/ImageGallery.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState } from "react";
+
+import { Gallery } from "react-grid-gallery";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+function ImageGallery({ images }) {
+
+    const [index, setIndex] = useState(-1);
+
+    const handleClick = (index, item) => setIndex(index);
+
+
+    const slides = images.map((image) => (
+        {
+            src: image.attributes.url,
+            thumbnailCaption: image.attributes.alternativeText,
+        }
+    ))
+
+    return (
+        <div>
+            <Gallery
+                images={slides}
+                onClick={handleClick}
+                enableImageSelection={false}
+            />
+            <Lightbox
+                slides={slides}
+                open={index >= 0}
+                index={index}
+                close={() => setIndex(-1)}
+            />
+        </div>
+    )
+}
+
+export default ImageGallery
--- app/works/[id]/page.jsx
+++ app/works/[id]/page.jsx
@@ -1,4 +1,5 @@
 import ContentHeader from "@/app/_components/ContentHeader"
+import ImageGallery from "@/app/_components/ImageGallery"
 import MDContent from "@/app/_components/MDContent"
 import NothingFound from "@/app/_components/NothingFound"
 import { getWorkById } from "@/app/_lib/apiCalls"
@@ -8,7 +9,7 @@ async function page({ params }) {
 
     const response = await getWorkById(params.id)
 
-    if(!response) return <NothingFound />
+    if (!response) return <NothingFound />
 
     return (
         <div className="p-4 w-full">
@@ -22,14 +23,9 @@ async function page({ params }) {
                 <MDContent content={response?.data?.attributes?.content} />
             </div>
 
-            {response?.data?.attributes?.images && response?.data?.attributes?.images?.data?.map((image) => (
-                <div key={image.id}>
-                    <Image src={image.attributes.url} width={150} height={150} alt={image.attributes.alternativeText} />
-                    <p>{image?.attributes?.caption}</p>
-                </div>
-            ))}
-
-            {/* {JSON.stringify(response?.data?.attributes?.images?.data, null, 2)} */}
+            {response?.data?.attributes?.images?.data && (
+                <ImageGallery images={response?.data?.attributes?.images?.data} />
+            )}
 
         </div>
     )
