ad68855c91306588457c177b9c18254e79f18111
ratigumashvili <ratigumashvili@gmail.com>
2024-08-30 11:09:37 +0400

    cleanup

--- app/_components/MDContent.jsx
+++ app/_components/MDContent.jsx
@@ -2,9 +2,11 @@
 
 import ReactMarkdown from 'react-markdown'
 
-function MDContent({ content }) {
+function MDContent({content}) {
   return (
-    <ReactMarkdown>{content}</ReactMarkdown>
+    <div className='[&_p]:mb-4 [&_p]:leading-7 font-light mb-4'>
+        <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
   )
 }
 
