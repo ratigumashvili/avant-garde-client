3d0588e08d5c65bb1695759696f9329df2cf1cb7
ratigumashvili <ratigumashvili@gmail.com>
2024-09-02 11:23:27 +0400

    cleanup

--- app/_lib/apiCalls.js
+++ app/_lib/apiCalls.js
@@ -12,7 +12,7 @@ export const getAllWorks = (params) => axios.get(`${apiUrl}/works?populate[0]=au
 
 export const getWorkByCategory = (author, category, params) => axios.get(`${apiUrl}/works?filters[$and][0][authors][slug][$eq]=${author}&filters[$and][1][category][slug][$eq]=${category}&populate=*&${params}`).then((resp) => resp.data).catch((error) => error.message)
 
-export const getWorkById = (id) => axios.get(`${apiUrl}/works/${id}?populate[0]=authors&populate[1]=images`).then((resp) => resp.data).catch((err) => console.log(err)).catch((error) => error.message)
+export const getWorkById = (id) => axios.get(`${apiUrl}/works/${id}?populate[0]=authors&populate[1]=images`).then((resp) => resp.data).catch((error) => error.message)
 
 export const getWorkByTitle = (title, params) => axios.get(`${apiUrl}/works?filters[$and][0][title][$contains]=${title}&populate=*&${params}`).then((resp) => resp.data).catch((error) => error.message)
 
@@ -22,7 +22,7 @@ export const getWorkByAuthor = (author, params) => axios.get(`${apiUrl}/works?fi
 
 export const getSingleManifest = (slug) => axios.get(`${apiUrl}/manifests?filters[$and][0][slug][$eq]=${slug}&populate=*`).then((resp) => resp.data).catch((error) => error.message)
 
-export const getManifestById = (id) => axios.get(`${apiUrl}/works/${id}?populate[0]=authors`).then((resp) => resp.data).catch((err) => console.log(err)).catch((error) => error.message)
+export const getManifestById = (id) => axios.get(`${apiUrl}/works/${id}?populate[0]=authors`).then((resp) => resp.data).catch((error) => error.message)
 
 export const getNoAuthorWork = (params) => axios.get(`${apiUrl}/works?filters[$and][0][category][slug][$eq]=uavtoro&${params}`).then((resp) => resp.data).catch((error) => error.message)
 
