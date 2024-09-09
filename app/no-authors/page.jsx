e9276cab1e9989ae003d218f8a1652602e554597
ratigumashvili <ratigumashvili@gmail.com>
2024-09-09 22:09:58 +0400

    minor fixes

--- app/no-authors/page.jsx
+++ app/no-authors/page.jsx
@@ -15,11 +15,10 @@ async function NoAuthors({ searchParams }) {
   return (
     <div className="p-4 w-full flex flex-col">
       <h2 className="font-gordeziani text-4xl font-light mb-4">უავტორო ჩანაწერები</h2>
-
       <div className="h-full">
 
         <ul className='h-full'>
-          {response?.data?.length && response.data.map((work) => (
+          {response?.data?.length !== 0 && response.data.map((work) => (
             <li key={work.id} className='my-2 list'>
               <Link href={`/works/${work.id}`}>{work.attributes.title}</Link>
             </li>
